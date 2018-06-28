018da555be1ab2ceb0b082ece03b1f3a9afe48a1
Mathis Loïc Messager <messamat@uw.edu>
2018-06-27 17:16:52 -0700

    Explore data prior to determining trees

--- seattle_preliminary.R
+++ seattle_preliminary.R
@@ -2,52 +2,92 @@ library(ggplot2)
 library(data.table)
 library(foreign)
 
+setwd("C:/Mathis/ICSL/stormwater/src/stormwater_samplingR")
+source('internal_functions.R')
+
 setwd("C:/Mathis/ICSL/stormwater/results/")
-BLMtrees <- read.dbf('BLMtrees_tab.dbf') #Traffic values for Big Leaf Maple
-NMtrees <- read.dbf('NMtrees_tab.dbf') #Traffic values for Norway Maple
+trees <- read.dbf('trees_tab.dbf') #Traffic values for Big Leaf Maple
 aadt <- read.dbf('heat_aadt.dbf') #Raster attribute table for Average Annual Daily Traffic heat map (AADT)
 spdlm <- read.dbf('heat_spdlm.dbf') #Raster attribute table for speed limit heat map
 streets <- read.dbf('Seattle_roads.dbf')
 
-#Function to bin raster attribute table
-bin_rastertab<- function(tab, binsize) {
-  minbin = min(tab$Value)-min(tab$Value)%%binsize+binsize
-  maxbin = max(tab$Value)-max(tab$Value)%%binsize+binsize
-  bins <- data.frame(bin=c(with(tab,rep(seq(minbin,maxbin, binsize),each=binsize)),maxbin),
-                     val=with(tab,seq(minbin-binsize,maxbin)))
-  tab_hist <- merge(tab, bins, by.x='Value', by.y='val')
-  tab_hist <- setDT(tab_hist)[,sum(Count), .(bin)]
-}
+########################################################################################################################
+# Format data
+########################################################################################################################
+str(trees)
+#Create industrial zoning boolean variable
+trees[trees$CLASS_DESC=='Manufacturing/Industrial' & !is.na(trees$CLASS_DESC),'industrial'] <- 1
+trees[trees$CLASS_DESC!='Manufacturing/Industrial' | is.na(trees$CLASS_DESC),'industrial'] <- 0
+#Compute tree density and plot % white people vs. density
+setDT(trees)[,treekm2:=length(UNITID)*1000000/ALAND10, .(GEOID10)]
+ggplot(trees, aes(x=DP0080003/DP0080001, y=treekm2)) + geom_point()
+#
+treesel <- trees[grepl('^Acer platanoides.+', trees$SCIENTIFIC) | trees$SCIENTIFIC=='Acer macrophyllum',]
+treesel$SCIENTIFIC_simple <- treesel$SCIENTIFIC
+treesel[grepl('^Acer platanoides.+', treesel$SCIENTIFIC), 'SCIENTIFIC_simple'] <- 'Acer platanoides' 
 
 ########################################################################################################################
 # Analyze distribution of traffic values for trees vs. whole city
 ########################################################################################################################
-#-----------------BIG LEAF MAPLE --------------
 #Speed limit
-ggplot(BLMtrees, aes(x=heat_spdlm)) +
-  geom_ribbon(data=spdlm, aes(x=Value, ymin=0, ymax=Count/10),stat='identity', fill='darkgrey') +
-  geom_histogram(fill='red', bins=100, alpha=1/2)+
-  geom_histogram(data=NMtrees, fill='blue', bins=100, alpha=1/5)+
-  scale_y_sqrt() + 
+spdlm$Value <- spdlm$Value+1
+spdlm_hist <- bin_rastertab(spdlm, 50, tukey=T, rep=10)
+setDT(spdlm_hist)[,binmin:=shift(binsback, 1, type='lag'),]
+spdlm_hist[1,'binmin'] <- min(spdlm$Value)
+ggplot() +
+  geom_rect(data=spdlm_hist, aes(xmin=binmin, xmax=binsback, ymin=0, ymax=count/100),fill='lightgrey')+
+  geom_histogram(data=treesel,aes(x=heat_spdlm, fill=SCIENTIFIC_simple), bins=100, alpha=1/2, position='identity')+
+  scale_y_sqrt(expand=c(0,0)) + 
+  scale_x_log10() + 
   theme_classic()
 
-#AADT
-aadt_hist <- bin_rastertab(aadt, 200)
-ggplot(BLMtrees, aes(x=heat_AADT)) +
-  geom_ribbon(data=aadt_hist, aes(x=bin, ymin=0, ymax=V1/10),stat='identity', fill='darkgrey') +
-  geom_histogram(fill='red', bins=100, alpha=1/3)+
-  geom_histogram(data=NMtrees, fill='blue', bins=100, alpha=1/5)+
+#AADT with logarithmic bins
+aadt$Value <- aadt$Value+1
+aadt_hist <- bin_rastertab(aadt, 50, tukey=T, rep=10)
+setDT(aadt_hist)[,binmin:=shift(binsback, 1, type='lag'),]
+aadt_hist[1,'binmin'] <- min(aadt$Value)
+ggplot() +
+  geom_rect(data=aadt_hist, aes(xmin=binmin, xmax=binsback, ymin=0, ymax=count/100),fill='lightgrey')+
+  geom_histogram(data=treesel,aes(x=heat_AADT, fill=SCIENTIFIC_simple), bins=100, alpha=1/2, position='identity')+
   scale_y_sqrt(expand=c(0,0)) + 
-  scale_x_continuous(expand=c(0,0))+
+  scale_x_log10() + 
   theme_classic()
+
+#######################################################################################
+#Create sampling design
+
+bin_rastertab(aadt, 50, tukey=T, rep=10)
+
+
+
+
 
 
 #######################################################################################
 # Analyze how functional class relates to AADT and Speed limit
 #http://streetsillustrated.seattle.gov/street-types/street-classification/
 
+str(streets)
+#Order factors
+streets$ARTDESCRIP <- factor(streets$ARTDESCRIP, 
+                             levels=c('Not Designated', 'Collector Arterial','Minor Arterial', 'Principal Arterial', 'State Route/Freeway','Interstate/Freeway'))
 
+#Arterial classification only slightly discriminate among speed limits
+ggplot(streets[!(streets$ARTDESCRIP %in% c('County Arterial', NA)),], aes(x=SPEEDLIMIT, fill=ARTDESCRIP)) + 
+  geom_histogram()+
+  facet_wrap(~ARTDESCRIP, scales='free_y', ncol=1) +
+  theme_bw()
 
+ggplot(streets[!(streets$ARTDESCRIP %in% c('County Arterial', NA)),], aes(x=ARTDESCRIP, y=SPEEDLIMIT, fill=ARTDESCRIP)) + 
+  geom_boxplot() +
+  theme_classic()
 
+#Arterial classification  only slightly discriminate among speed limits
+ggplot(streets[!(streets$ARTDESCRIP %in% c('County Arterial', NA)),], aes(x=ARTDESCRIP, y=AADT_inter, fill=ARTDESCRIP)) + 
+  geom_boxplot() + 
+  scale_y_log10() + 
+  theme_classic()
 
+##############################################################################################
+#Devise multivariate sampling procedure
 
